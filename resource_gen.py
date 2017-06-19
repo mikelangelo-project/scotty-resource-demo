c585347c93bee7110aca7bc446e9b684a15b4955
msrba <maik.srba@gwdg.de>
2017-06-19 14:47:23 +0200

    Update resource_gen.py

--- resource_gen.py
+++ resource_gen.py
@@ -3,8 +3,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 def deploy(context):
-    name = context.v1.resource_config['name']
+    name = context.v1.self.name
     print 'Hey there,'
     print 'my name is {name} '.format(name=name),
     print 'and I\'m deploy a dummy resource with my config:'
-    print '{config}'.format(config=context.v1.resource_config)
+    print '{config}'.format(config=context.v1.self.config)
