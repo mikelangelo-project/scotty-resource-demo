d5f0941c089bc984773e495fe4c31eeb701e8238
Maik Srba <msrba@gwdg.de>
2017-09-04 10:35:30 +0200

    update demo

--- resource_gen.py
+++ resource_gen.py
@@ -1,22 +1,21 @@
 import logging
 
-logger = logging.getLogger(__name__)
+from scotty import utils
 
+logger = logging.getLogger(__name__)
 
 def endpoint(context):
     endpoint = {
-        'url': 'https://demo.resouces',
+        'url': 'https://demo.resource',
         'user': 'scotty',
-        'password': '********',
+        'password': '********'
     }
     return endpoint
 
 def deploy(context):
-    name = context.v1.resource.name
-    print 'Hey there,'
-    print 'my name is {} '.format(name),
-    print 'and I\'m deploy a dummy resource with my config:'
-    print '{}'.format(context.v1.resource.config)
-    
+    resource = context.v1.resource
+    logger.info('Hey there,')
+    logger.info('I\'m resource generator {}'.format(resource.name))
+
 def clean(context):
     pass
